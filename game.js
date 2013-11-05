264c1edfe9fb0b08b209a37574674c2cc8a7004f
Sergio <sergiolindo.empresa@gmail.com>
2013-11-05 11:40:46 +0100

    fix ID

--- game.js
+++ game.js
@@ -28,7 +28,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-45433616-1', 'binary-sequence.github.io');
+ga('create', 'UA-45433616-2', 'binary-sequence.github.io');
 ga('send', 'pageview');
 
 
